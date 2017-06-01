39cbcc9595cabf159b53d7107d734e5b4ca1df0a
Briansc7 <samudio.utn@gmail.com>
2017-06-01 18:29:05 -0300

    Cambio en los Test que daban error, y en se quitaron cosas que no se usaban en los test

--- src/test/java/modelTest/OperacionesParserConCuentas.java
+++ src/test/java/modelTest/OperacionesParserConCuentas.java
@@ -16,25 +16,17 @@ import model.Planilla;
 import viewModel.PrincipalViewModel;
 
 public class OperacionesParserConCuentas {
-	Indicador resultado; 
+	Indicador resultado;
 	@Before
 	public void initialize(){
-		//prueba = new Indicador("prueba","10+56");
+		Empresa empresa = new Empresa("Facebook", Arrays.asList(new Cuenta("ebitda", 500, "2015-01-21")));
+		Planilla.instance.setEmpresaElegida(empresa);
+		Planilla.instance.setPeriodoElegido(2015);
 	}
 	
 	@Test
 	public void leerUnaCuenta() throws NumberFormatException, ParseException, TokenMgrError, NullPointerException, IOException, parser.ParseException, parser.TokenMgrError{
-		PrincipalViewModel principalViewModel = new PrincipalViewModel();
-		principalViewModel.setPath(".\\Archivos de prueba\\baseDatosPrueba.csv");
-		//System.out.println(principalViewModel.getPath());
-		principalViewModel.verificarArchivo();
-		
-		//ConsultarCuentasViewModel consultarCuentasViewModel = new ConsultarCuentasViewModel("Facebook");
-		//consultarCuentasViewModel.setPeriodoElegido(2015);
-		Empresa empresa = new Empresa("Facebook", Arrays.asList(new Cuenta("ebitda", 500, "2015-01-21"))); 
-		Planilla.instance.setEmpresaElegida(empresa);
-		Planilla.instance.setPeriodoElegido(2015);
-		
+
 		resultado = new Indicador("suma","c.Ebitda");//fb ebitda 2015
 		Assert.assertEquals(500,resultado.getValor());
 	}
