1c8f86d9e4305f9d9a65d4591694e327ae4ec2ff
Ezequiel <ezequiel_luppino@live.com>
2017-07-07 15:35:01 -0300

    Inicializadado el guardado de Metodologias

--- src/main/java/model/Ejecutable.java
+++ src/main/java/model/Ejecutable.java
@@ -3,12 +3,16 @@ package model;
 import org.uqbar.arena.Application;
 import org.uqbar.arena.windows.Window;
 
+import dtos.PathFileTxtJson;
+import utils.AppData;
 import view.PrincipalView;
 
 public class Ejecutable extends Application{
 	
 	public static void main(String[] args) {
 		//Planilla.instance.agregarIndicador(new Indicador("a", "c.van + 100"));
+		
+		AppData.getInstance().setInicializacionMetodologias(new PathFileTxtJson("./Archivos del sistema/Metodologias.txt"));
 		
 		new Ejecutable().start();
 		
