64185b102ba3e6cd19daab489325ab06d1994bcd
Briansc7 <samudio.utn@gmail.com>
2017-07-08 11:31:33 -0300

    Comentarios sobre corrección al final de archivo ConsultarMetodologiasViewModel

--- src/main/java/viewModel/ConsultarMetodologiasViewModel.java
+++ src/main/java/viewModel/ConsultarMetodologiasViewModel.java
@@ -79,3 +79,41 @@ private BaseDeDatos baseDeDatos;
 		this.nombreMetodologiaElegida = nombreMetodologiaElegida;
 	}
 }
+
+
+//hacer a las condiciones POLIMORFICAS. Tener una unica lista de condiciones
+//Al aplicar la condicion a la metodologia te retorne la lista (mas chica u ordenada)
+/*
+probar taxativa y no taxativa -> usar mejores nombres porque genera confusion (algo para diferenciar que es filtro u orden)
+
+test comparar que listas sean iguales. si a tiene los elementos de b, y b los de a, las listas son iguales
+
+Importante crear un builder
+
+no usar strings en lugar de usar los objetos.
+
+tener en cuenta si pide periodo de 10 año y tiene 3 años o tiene huecos (se olvidó de cargar 2016).
+
+Customizar la lista de empresas para decir si conviene invertir, el orden, si no aplica, etc
+
+condicion conozca a funcion promedio, reciba una empresa, un indicador y la cantidad de periodos.
+roe de los ultimos 5 periodos, ejemplo que haga sumatoria (funcion que actua sobre el indicador).
+funcion va a devolver un solo valor que se lo aplica a condicion.
+
+
+
+tarea
+diagrama de clases completo
+
+no guardar como strings las cosas, trabajar con la referencia exacta
+
+una sola lista para las metodologias. Interfaz que entienda los mismos metodos al aplicar la condicion a la metodologia.
+interfaz va a servir para cuando hagas una lista hagas una lista de esa interfaz. A todo lo que haya que hacer polimorfico lo mas prolijo es usar interfaz
+
+separar test de operaciones, de metodologias, de condiciones, de funciones.
+
+Usar builder para por ejemplo hacer mas chico las cosas en las metodologias o usar una abstraccion.
+
+Al iniciar la aplicacion que se cargue automaticamente el archivo con las empresas
+
+*/
