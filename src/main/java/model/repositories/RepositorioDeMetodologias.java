9b96f9865df8f1ae9a2de739cca0a85515c0e6a2
Ezequiel <ezequiel_luppino@live.com>
2017-09-14 14:48:28 -0300

    Modificado el RepositorioDeMetodologias para que se adapte al modelo nuevo

--- src/main/java/model/repositories/RepositorioDeMetodologias.java
+++ src/main/java/model/repositories/RepositorioDeMetodologias.java
@@ -1,29 +1,52 @@
 package model.repositories;
 
-import java.math.BigDecimal;
-import java.util.ArrayList;
-import java.util.Arrays;
-import java.util.Comparator;
 import java.util.List;
-import java.util.stream.Collectors;
+
+import javax.persistence.EntityTransaction;
 
 import org.uqbarproject.jpa.java8.extras.WithGlobalEntityManager;
 
-import exceptions.EseYaExisteException;
-import mockObjects.AppDataI;
 import model.Metodologia;
-import model.funciones.Consistencia;
-import model.funciones.Longevidad;
-import model.funciones.ValoresDelPeriodo;
-import model.metodologia.condiciones.Comparador;
-import model.metodologia.condiciones.CondicionGeneral;
-import testMetodologia.CondicionBuilder;
-import utils.AppData;
 
 public class RepositorioDeMetodologias implements WithGlobalEntityManager{
 	private static RepositorioDeMetodologias instance;
-	private List<Metodologia> metodologias = new ArrayList<Metodologia>();
-	private AppDataI appData = AppData.getInstance();
+	private EntityTransaction transaction = entityManager().getTransaction();
+	
+	//Singleton
+	private RepositorioDeMetodologias(){}
+	
+	public synchronized static RepositorioDeMetodologias getInstance(){
+		if(instance == null)
+			instance = new RepositorioDeMetodologias();
+		return instance;
+	}
+
+	public void agregarMetodologias(List<Metodologia> _metodologias) {
+		_metodologias.stream().forEach(metodologia -> guardarMetodologia(metodologia));
+	}
+	
+	public void guardarMetodologia(Metodologia unaMetodologia) {
+		transaction.begin();
+		entityManager().persist(unaMetodologia);
+		transaction.commit();
+	}
+
+	public void removerMetodologia(Metodologia metodologia) {
+		transaction.begin();
+		entityManager().remove(metodologia);
+		transaction.commit();
+	}
+	
+	public List<Metodologia> obtenerMetodologias(String nombre) {
+		@SuppressWarnings("unchecked")
+		List<Metodologia> metogologias = entityManager()
+				.createQuery("select metodologia from Metodologia as metodologia where metodolgoia.nombre = ?1")
+				.setParameter(1, nombre)
+				.getResultList();
+		
+		return metogologias;
+	}
+
 	/*
 	//private CondicionGeneral condicionRoe = new CondicionNoTaxativa(2, "ROE", new GreaterThan(), 1);
 	private CondicionGeneral condicionRoe = new CondicionBuilder()
@@ -58,89 +81,5 @@ public class RepositorioDeMetodologias implements WithGlobalEntityManager{
 			.valor(new BigDecimal(5))
 			.build();
 	private Metodologia buffet = new Metodologia("Buffet", Arrays.asList(condicionMargen, condicionLongevidadPropia, condicionRoe, condicionDeuda, condicionLongevidadComparativa));	
-	*/
-	//Singleton
-	private RepositorioDeMetodologias(){
-		//metodologias.add(buffet);
-		archivarRepositorio();
-	}
-	
-	public synchronized static RepositorioDeMetodologias getInstance(){
-		if(instance == null)
-			instance = new RepositorioDeMetodologias();
-		return instance;
-	}
-	
-	public void inicializar(List<Metodologia> _metodologias){
-		metodologias.addAll(_metodologias);
-	}
-
-	public void agregarMetodologias(List<Metodologia> _metodologias) {
-		_metodologias.stream().forEach(metodologia -> agregarMetodologia(metodologia));
-	}
-	
-	public void agregarMetodologia(Metodologia metodologia) {
-		comprobarNombre(metodologia);
-		metodologias.add(metodologia);
-		archivarRepositorio();
-	}
-
-	public void removerMetodologia(Metodologia metodologia) {
-			metodologias.remove(metodologia);
-			archivarRepositorio();
-	}
-	
-	// Filtrar metodologias del repositorio
-	public List<Metodologia> filtrarPorNombre(String nombre) {
-		List<Metodologia> _metodologias;
-		
-		_metodologias = metodologias.stream().filter(
-				metodologia -> metodologia.getNombre().contains(nombre))
-				.collect(Collectors.toList());
-		
-		/*if(_metodologias.isEmpty())
-			throw new NoSeEncuentraException("La metodologia con nombre: \""
-						+ nombre
-						+ "\" no se encuentra en el repositprio");*/
-
-		return _metodologias;
-	}
-
-	// Devuelven una lista ordenada de determinada manera, sin alterar las
-	// propias del repositorio
-	public List<Metodologia> getOrdenadasPorNombre() {
-		List<Metodologia> _metodologias = metodologias.stream()
-				.sorted(Comparator.comparing(Metodologia::getNombre))
-				.collect(Collectors.toList());
-		return _metodologias;
-	}
-	
-	//Utilidades
-	public int size() {
-		return metodologias.size();
-	}
-	
-
-	public void limpiarRepositorio() {
-		metodologias = new ArrayList<Metodologia>();
-	}
-
-	private void archivarRepositorio() {
-		appData.guardarMetodologias(metodologias);
-	}
-	
-	//Setters y getters
-	public List<Metodologia> getMetodologias(){
-		return metodologias;
-	}
-	
-	public void setAppData(AppDataI _appData){
-		appData = _appData;
-	}
-	
-	private void comprobarNombre(Metodologia metodologia) {
-		if(metodologias.contains(metodologia))
-			throw new EseYaExisteException("Ya existe una metodologia con el nombre: "
-					+ metodologia.getNombre());
-	}
+	 */
 }
