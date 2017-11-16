401c1117f52765df893ef647425d8923de37fda5
Briansc7 <samudio.utn@gmail.com>
2017-11-16 11:13:01 -0300

    Comienzo de agregado de ejecutable de comandos sql

---
+++ src/main/java/model/EjecutarCmdSQL.java
@@ -0,0 +1,13 @@
+package model;
+
+import java.io.FileNotFoundException;
+
+public class EjecutarCmdSQL {
+
+	private static String PATH_SQL = "./Archivos del sistema/cmdsql.txt";
+
+    public static void main(String[] args) throws FileNotFoundException {
+    	
+    }
+	
+}
